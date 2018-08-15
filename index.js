8c6b604d6791768b7157e644dd16b007a2341ce9
manon jacquin <manon.jacquin@gmail.com>
2018-08-15 21:46:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,4 +14,6 @@ function deleteFromObjectByKey(object, key){
   return delete newObj[key];
 }
 
-function 
+function destructivelyDeleteFromObjectByKey(object, key){
+  return delete object[key];
+}
